72d3aed52e2282374aae7f254eb89ea270fb5f7d
Ivan Dmitriev <42055372+IvanDmitriev1@users.noreply.github.com>
2022-11-11 16:53:41 +0600

    added FileOpenPickerExtensions

---
+++ WinUi.Essentials/Extensions/FileOpenPickerExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Windows.Storage.Pickers;
+using WinUIEx;
+
+namespace WinUi.Essentials.Extensions;
+
+public static class FileOpenPickerExtensions
+{
+    public static FileOpenPicker CreateFileOpenPicker()
+    {
+        var picker = new FileOpenPicker();
+        WinRT.Interop.InitializeWithWindow.Initialize(picker, ApplicationEx.MainWindow.GetWindowHandle());
+
+        return picker;
+    }
+}
