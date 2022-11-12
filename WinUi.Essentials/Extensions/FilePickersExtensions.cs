147fb9f9277e0a63a28330f4c4b84e7cc196d091
Ivan Dmitriev <42055372+IvanDmitriev1@users.noreply.github.com>
2022-11-12 18:05:46 +0600

    added CreateFileSavePicker

---
+++ WinUi.Essentials/Extensions/FilePickersExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Windows.Storage.Pickers;
+using WinUIEx;
+
+namespace WinUi.Essentials.Extensions;
+
+public static class FilePickersExtensions
+{
+    public static FileOpenPicker CreateFileOpenPicker(string fileType)
+    {
+        var picker = new FileOpenPicker();
+        WinRT.Interop.InitializeWithWindow.Initialize(picker, ApplicationEx.MainWindow.GetWindowHandle());
+        picker.FileTypeFilter.Add(fileType);
+
+        return picker;
+    }
+
+    public static FileSavePicker CreateFileSavePicker(string suggestedFileName, string key, IList<string> values)
+    {
+        var picker = new FileSavePicker();
+        WinRT.Interop.InitializeWithWindow.Initialize(picker, ApplicationEx.MainWindow.GetWindowHandle());
+        picker.SuggestedFileName = suggestedFileName;
+        picker.FileTypeChoices.Add(key, values);
+
+        return picker;
+    }
+}
